686205ebb1df6cbca1a1d14b2b6d4091f6f1cca0
Jonas S Karlsson <jsk@yesco.org>
2021-08-07 05:27:01 +0700

    darr cleanup and comments

--- Play/darr.c
+++ Play/darr.c
@@ -1,3 +1,41 @@
+//
+//   darr.c - Dynamic ARRay in C
+//
+// (>) CC-BY 2021 Jonas S Karlsson
+//         jsk@yesco.org
+//
+
+// Dynamic array
+// =============
+// This is a dynamic growable managed array of dint:s (a long, or pointer!).
+// It grows as need at put.
+// Notice that all update functions take and return the array
+// (potentially new realloced!)
+//
+//   dint v;
+//   darr* d= NULL;
+//   d= dput(d, index, value); // this reallocs!
+//   v= dget(d, index);
+//   printf("len=%d\n", dlen(d));
+//
+// 
+// Interally, it keeps track allocated memory, the first index, and last index.
+// This allows it to also act as a stack, and a queue:
+//
+//   darr* d= NULL;
+//   d= dpush(d, value);
+//   ...
+//   v= dpop(d);
+//   printf("len=%d\n", dlen(d));
+//
+// And as a queue:
+//   d= dpush(d, value);
+//   v= dshift(d, value);
+// or
+//   d= dunshift(d, value);
+//   v= dpop(d):
+//
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -23,43 +61,54 @@ darr* darray(darr* d, int capacity) {
   return d;
 }
 
+// get length of active items in D array
 int dlen(darr* d) {
   return !d ? 0 : d->next - d->first;
 }
 
-// from D array at index I get data
-dint dget(darr* d, int i) {
-  if (i<0) i=d->next+i; // from end
-  if (!d || i<0 || i >= d->next) return 0; // error
-  return d->data[i];
+// don't use directly; unsafe
+dint* _dptr(darr* d, int i) {
+  if (i<0) i= d->next+i; // from end
+  return i<0 ? NULL : &d->data[i];
 }
 
 // in D array, put DATA at index I 
-// returns old_val || 0
+// -n means n:th from the end
+// Usage: d= dput(d, 42, 4711);
 darr* dput(darr* d, int i, dint data) {
-  if (i<0) i=d->next+i; // from end
-  if (i<0) return d; // error - ignore!
   d= darray(d, i);
-  d->data[i]= data;
-  if (i >= d->next) d->next = i+1;
+  *(_dptr(d, i))= data;
+  if (i >= d->next) d->next= i+1;
   return d;
+}
+
+// from D array at index I get data
+// -n means n:th from the end
+// Usage: v= dget(d, 42); // => 4711
+dint dget(darr* d, int i) {
+  dint* p= _dptr(d, i);
+  return p ? *p : 0;
 }
 
 // stack using: dpush, dpop
 // circular with: dunshift, dshift
-// WARNING: dput/dget indices are not what you expect
+
+// WARNING: dput/dget indices are not what you expect (0th at offset d->first)
+
+// Usage: d= dpush(d, 99);
 darr* dpush(darr* d, dint data) {
   return dput(d, d->next, data);
 }
 
+// Usage: v= dpop(d); // => 99
 dint dpop(darr* d) {
-  if (d->next == d->first) return 0;
-  dint val= dget(d, d->next-1);
-  dput(d, d->next-1, 0);
-  d->next--;
-  return val;
+  if (d->first >= d->next) return 0;
+  dint* p= _dptr(d, --d->next);
+  dint v= *p; *p= 0;
+  return v;
 }
 
+// Usage: d= dunshift(d, 42);
 darr* dunshift(darr* d, dint data) {
   if (--d->first < 0) {
     darray(d, d->max + DARR_STEP);
@@ -71,14 +120,14 @@ darr* dunshift(darr* d, dint data) {
   return dput(d, d->first, data);
 }
 
+// Usage: v= dnshift(d) // ==> 42
 dint dshift(darr* d) {
   if (d->first >= d->next) return 0;
+  dint* p= _dptr(d, d->first++);
+  dint v= *p; *p= 0;
   // TODO: shrink if used as circular...
- // if (d->first > 2*DARR_STEP) ...
-  dint val= dget(d, d->first);
-  dput(d, d->first, 0);
-  d->first++;
-  return val;
+  // if (d->first > 2*DARR_STEP) ...
+  return v;
 }
 
 /* ENDWCOUNT */
