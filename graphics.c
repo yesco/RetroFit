9282e53fcb33b613d147e503d7fea3db813ea13f
Jonas S Karlsson <jsk@yesco.org>
2021-08-26 06:22:08 +0700

    - adding a IBM retro font for rendering BIG ANSI ASCII

--- graphics.c
+++ graphics.c
@@ -6,6 +6,10 @@
 // TOOD: make independent?
 // User extensible scripts!
 
+#ifdef MAIN
+  #include "jio.c"
+#endif
+
 void drawX() {
   for(int rr=1; rr<=screen_rows-2; rr++) {
     //B(white);
@@ -42,3 +46,55 @@ void drawPullDownMenu(color *colors, char **labels, int n) {
   }
 }
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Graphics/font8x8/font8x8_basic.h"
+
+void plot(int r, int c, char ch, int w, int h, int bg, int fg) {
+  //printf("plot %d %d %c\n", r, c, ch); return;
+  if (ch>127) return;
+  char *def = font8x8_basic[ch];
+
+  for (int y=0; y < 8; y++) {
+    //if (y==3 || y==5 || y==7) {r--;continue;}
+    gotorc(r+y, c);
+    char bits=def[y];
+    for (int x=0; x < 8; x++) {
+      //if (x==1 || x==4 || x==2) continue;
+      B(bg);
+      if (bits & 1 << x)
+        B(fg);
+      else
+        B(bg);
+      putchar(' ');
+    }
+  }
+}
+
+void plots(int r, int c, char *s, int w, int h, int bg, int fg) {
+  int ch;
+  while((ch= *s++)) {
+    if (ch=='\n' || c+8>=screen_cols) {
+      r+= 8;
+      c = 0;
+    }
+    if (ch=='\n') continue;
+    plot(r, c, ch, w, h, bg, fg);
+    if ((c+= 8)>=screen_cols) {
+      r+= 8;
+      c = 0;
+    }
+  }
+}
+
+#ifdef MAIN
+int main(void) {
+  screen_init();
+  if (0)
+    plots(0, 0, "Jonas S Karlsson\nis my name", 1, 1, black, yellow);
+  else
+    plots(0, 0, "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~", 1,1, black, yellow);
+  printf("\n");
+}
+#endif
--- jio.c
+++ jio.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 #include <termios.h>
 #include <time.h>
 #include <locale.h>
