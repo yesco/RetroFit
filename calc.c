57ff8fd2e2613c42a4b873b51d9a78cedbe579d8
Jonas S Karlsson <jsk@yesco.org>
2024-07-28 23:28:35 +0700

    calc: actually calculate by keystroke

--- calc.c
+++ calc.c
@@ -1,8 +1,36 @@
 #include "jio.c"
 #include "graphics.c"
 #include <math.h>
+#include <assert.h>
+
+#define NKEYS 64
+
+char keyc[NKEYS]= {0};
+int keyy[NKEYS]= {0};
+int keyx[NKEYS]= {0};
+int nkeys= 1; // ahum, not 0?
+
+// Search key positions to find where clicked
+// Returns: 0 if not found, otherwise index
+int findnkey(int x, int y) {
+  int i;
+  for(i=0; i<NKEYS; i++) { 
+    if (x < keyx[i]) break;
+    if (y < keyy[i]) break;
+    return i;
+  }
+  return 0;
+}
 
 void dkey(char k) {
+  // remember coordinates
+  nkeys++;
+  assert(nkeys<NKEYS);
+  keyc[nkeys]= k;
+  keyx[nkeys]= gx;
+  keyy[nkeys]= gy;
+
+  // draw
   gputc(' ');
 
   int x= gx, y= gy;
@@ -40,21 +68,56 @@ void row(char* keys) {
 }
 
 void calc() {
+  int firsttime= 1;
   gcanvas();
 
   // clear actual screen
   wclear();
   
-  double stack[16]= {0}, *s= stack;
+  double stack[16]= {0}, *s= stack+1;
   int k;
   do {
-    double v= *s, b= v, a= s[-1];
+    gx= 0; gy= 0;
+    int wc= 6*3;
+    int w= wc*8;
+    // draw result panel
+    char r[64]= {0};
+    sprintf(r, "%32g", *s);
     
-    switch(k) {
-    case '0' ... '9': v= v*10+v; break;
+    gx= w-8*strlen(r);
+    gputs(r);
+    gputc('\n'); gy+= 4;
+
+    if (firsttime) {
+      // draw keys
+      row("( ) Q L X S");
+      row("7 8 9 * D C");
+      row("4 5 6 / B T");
+      row("1 2 3 - I ");
+      row(". 0 E + < >");
+      firsttime= 0;
+    }
+
+    gupdate();
+
+    // action by key
+    int k= key();
+
+    // take two -- see below
+    if (s-stack<2 && strchr("+-*/^", k)) continue;
+
+    // action
+    switch(toupper(k)) {
+    case '0' ... '9': *s= 10* *s + k-'0'; break;
     case '.': break; // TODO: enter string?
     case 'E': break; // TODO: 1E5
 
+    case ' ': case '\n': case '\r': // or <enter> or space
+    case '<': s++; *s= 0; break; // push/enter
+    case '>': if (s>stack) s--; break; // pop
+      // case ' TODO: dup??? 
+
+      // take two - see above
     case '+': s--; *s= *s+s[1]; break;
     case '-': s--; *s= *s-s[1]; break;
     case '*': s--; *s= *s*s[1]; break;
@@ -74,24 +137,7 @@ void calc() {
     case 'T': *s= tan(*s); break;
     }
 
-    gclear();
-
-    int w= 5*8*3;
-    // draw result panel
-    char* r= "4711";
-    gx= w-8*strlen(r);
-    gputs(r);
-    gputc('\n'); gy+= 4;
-
-    // draw keys
-    row("( ) Q L X S");
-    row("7 8 9 * D C");
-    row("4 5 6 / B T");
-    row("1 2 3 - I ");
-    row(". 0 < + =");
-
-    gupdate();
-  } while((k= key())!=CTRL+'C');
+  } while(k!=CTRL+'C');
 }
 
 int main(void) {
